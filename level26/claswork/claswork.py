58a7c48212289f37fcab5330f20195ef37c8bdc5
giorgikenchosvili <gkenchoshvili63@gmail.com>
2025-02-02 00:19:17 +0400

    commit

--- level26/claswork/claswork.py
+++ level26/claswork/claswork.py
@@ -1,1 +1,21 @@
-print("no claswork")
+names = ["Giorgi","Vano","Lika","Nika","Luka"]
+
+names2 = ["Andria","Giorgi","Paata","Vano"]
+
+
+
+extended_names = names+names2
+
+extended_names.insert(5, "Zauri")
+
+
+length = len(extended_names)
+
+
+
+
+
+print(extended_names.count("Giorgi"))
+
+
+print(extended_names.index("Giorgi"))
--- level26/homework/homework.py
+++ level26/homework/homework.py
@@ -1,1 +1,43 @@
-print("no homework")
+# ages ={ 15, 15, 16, 16, 16, 16, 16, 16, 15, 17, 17, 17, 17, 19, 21, 21, 23, 25, 25, 26, 32}
+
+# average_age = sum(ages) / len(ages)
+
+name_list = ["გიორგი", "ანა", "გიორგი", "მარიამი", "გიორგი"]
+my_name = "გიორგი"
+
+result = 0
+for name in name_list:
+    if name == my_name:
+        result += 1
+
+print(f"{my_name}{result} ")
+
+name_list = ["გიორგი", "ანა", "გიორგი", "მარიამი", "გიორგი"]
+my_name = "გიორგი"
+
+count = name_list.count(my_name)
+
+print(f"{my_name}{count}")
+
+
+import random
+
+
+random_numbers = [random.randint(1, 100) for _ in range(20)]
+print( random_numbers)
+
+
+even_numbers = [num for num in random_numbers if num % 2 == 0]
+print( even_numbers)
+
+
+for i in range(1, 11):
+  
+   
+    if i % 2 != 0:
+        print(i)
+
+names = ["თამარი", "გიორგი", "ანა", "თეონა", "მარიამი"]
+filtered_names = [name for name in names if not name.startswith('თ')]
+
+print( filtered_names)
