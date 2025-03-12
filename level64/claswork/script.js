2708302a224a32e804b164dd2b0dd89fa7a154b4
giorgikenchosvili <gkenchoshvili63@gmail.com>
2025-03-12 23:24:00 +0400

    commit

---
+++ level64/claswork/script.js
@@ -0,0 +1,16 @@
+console.log("giorgi kenchoshvili")
+
+var username = prompt("enter your youser name")
+var usersurname = prompt("enter your youser lastname")
+
+
+console.log("hello " + username   + " " + usersurname)
+
+
+var username = prompt("enter your youser name")
+
+alert("walcome " + username)
+
+
+
+
---
+++ level64/homework/script.js
@@ -0,0 +1,66 @@
+// 2) მომხმარებელს შემოატანინეთ 2 რიცხვი, ხოლო შემდეგ გაკეთეთ ამ რიცხვებზე ყველა მათემატიკური ოპერაცია ცალცალკე
+var num1 = prompt("enter first number:");
+var num2 = prompt("enter second number:");
+console.log(num1  +  num2 );
+console.log(num1 -  num2 );
+console.log(num1  * num2 );
+console.log(num1  /  num2 );
+
+
+var name = prompt("enter your name  :");
+var surname = prompt("enter your lastname  :");
+console.log(name + " " + surname)
+
+
+// 4) შექმენი ორი input შეადარე ორი რიცხვი და დაბეჭდე შედეგი.
+
+var num1 = prompt("enter first number:");
+var num2 = prompt("enter second number:");
+
+console.log(num1 > num2)
+
+
+// 5) შექმენი 2 ცვლადი შეამოწმე, ტოლია თუ არა ორი რიცხვი. 
+
+
+var num1 = prompt("enter first number:");
+var num2 = prompt("enter second number:");
+
+console.log(num1 == num2)
+
+
+
+// 6) მომხმარებელს შემოახანინე input შეამოწმე, არის თუ არა რიცხვი 50-ზე ნაკლები.
+
+var num1 = prompt("enter first number:");
+console.log(num1 <50)
+
+// 7)შემოიტანე 2 input შეამოწმე, არის თუ არა ორი რიცხვის ჯამი 100-ზე მეტი.
+
+
+var num1 = Number(prompt("enter first number:"));
+var num2 = Number(prompt("enter second number:"));
+console.log(num1 + num2 < 100)
+
+// 8) მომხარებელს შემოატანინე input შეამოწმე, არის თუ არა რიცხვი 10-ზე მეტი ან ტოლი.
+
+var num1 = prompt("enter first number:");
+console.log(num1 <=10)
+
+
+// 9)შემოიტანე 1 input და 1 ცლადი შეადარე, არის თუ არა ერთი რიცხვი მეორეზე ნაკლები ან ტოლი.
+var nmae = 10
+var num1 = prompt("enter first number:");
+console.log(num1 <= nmae)
+
+// 10) მომხმარებელს შემოატანინეთ სახელი და შეამოწმეთ ეს სახელი უდრის თუ არა თქვენს სახელს
+
+var name = prompt("enter your name  :");
+console.log(name = giogi)
+
+// 11) მომხმარებელს შემოატანინეთ პაროლი შემდეგ კითხეთ ხელახლა რომ გაიმეოროს პაროლი და შეამოწმეთ უდრის თუ არა ისინი ერთმანეთს -->
+
+var num1 = prompt("enter password:");
+var num2 = prompt("conform password:");
+
+console.log( num1== num2)
