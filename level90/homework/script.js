72cc884b09e51f5559d1e01185828f59022a758b
giorgikenchosvili <gkenchoshvili63@gmail.com>
2025-05-10 22:58:03 +0400

    commit

---
+++ level90/homework/script.js
@@ -0,0 +1,7 @@
+let cartt = document.getElementById("shopping")
+let opener =document.querySelector(".right")
+opener.addEventlistener("click", function(){
+    cartt.style.transform = "translateX(0)"
+})
+    
+    
